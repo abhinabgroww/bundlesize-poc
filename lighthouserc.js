f9d1b28d058c05cf9a939f865dec0652beb36d12
Abhinab Pratap Singh Chauhan <abhinabpsc@MacBook-Pro.local>
2023-09-21 04:57:01 -0700

    Action updated

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,30 +1,30 @@
-module.exports = {
-	ci: {
-		collect: {
-			url: 'http://localhost:3000',
-			startServerCommand: 'npm run start',
-		},
-		assert: {
-		  preset: 'lighthouse:recommended'
-		},
-		upload: {
-			target: 'temporary-public-storage',
-		},
-	},
-};
-
 // module.exports = {
 // 	ci: {
 // 		collect: {
-// 			url: [`https://${process.env.PREVIEW_URL}`],
-// 			startServerCommand: process.env.NODE_ENV === 'production' ? undefined : 'npm run start',
+// 			url: 'http://localhost:3000',
+// 			startServerCommand: 'npm run start',
 // 		},
 // 		assert: {
-// 			// preset: 'lighthouse:recommended',
-// 			preset: 'lighthouse:no-pwa',
+// 		  preset: 'lighthouse:recommended'
 // 		},
 // 		upload: {
 // 			target: 'temporary-public-storage',
 // 		},
 // 	},
 // };
+
+module.exports = {
+	ci: {
+		collect: {
+			url: [`https://${process.env.PREVIEW_URL}`],
+			startServerCommand: process.env.NODE_ENV === 'production' ? undefined : 'npm run start',
+		},
+		assert: {
+			// preset: 'lighthouse:recommended',
+			preset: 'lighthouse:no-pwa',
+		},
+		upload: {
+			target: 'temporary-public-storage',
+		},
+	},
+};
