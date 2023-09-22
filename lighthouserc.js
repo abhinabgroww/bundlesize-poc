3b3fed7bd71c42eeb1ba0ead6009ab552e60f1f8
Abhinab <abhinabpsc@groww.in>
2023-09-22 16:39:05 +0530

    Action updated

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,44 +1,28 @@
- 
 module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: "npm run start",
+      startServerReadyPattern: "ready on",
+      url: ["http://localhost:3000"],
 
-	"ci": {
-  
-		collect: {
-  
-			startServerCommand: "npm run start",
-  
-			startServerReadyPattern: "ready on",
-  
-			url: [
-  
-				"http://localhost:3000"
-  
-			],
-  
-			numberOfRuns: 1,
-  
-			settings: {
-  
-				preset: "desktop"
-  
-			}
-  
-		},
-  
-		assert: {
-      assertions: {
-        'categories:performance': ['warn', {minScore: 0.90}],
-        'categories:accessibility': ['error', {minScore: 0.80}]
-      }
+      numberOfRuns: 1,
+
+      settings: {
+        preset: "desktop",
+      },
     },
-		"upload": {
-  
-			"target": "temporary-public-storage"
-  
-		},
-  
-		"server": {}
-  
-	}
-  
-  };
+
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 0.8 }],
+      },
+    },
+	
+    upload: {
+      target: "temporary-public-storage",
+    },
+
+    server: {},
+  },
+};
