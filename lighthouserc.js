00a10c0502c7dd820482cf76fca4924134354c4e
Abhinab <abhinabpsc@groww.in>
2023-09-29 12:28:31 +0530

    Workflow updated

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,28 +1,53 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: "npm run start",
-      startServerReadyPattern: "ready on",
-      url: ["http://localhost:3000"],
+      method: "node",
+      headful: true,
+      url: [
+        "https://groww.in/",
+        // 'https://groww.in/download-forms',
+        // 'https://groww.in/blog',
+        // 'https://groww.in/blog/best-diwali-stocks-to-invest',
+        // 'https://groww.in/blog/best-fixed-deposit-in-india',
+        // 'https://groww.in/blog/best-gold-stocks-in-india',
+        // 'https://groww.in/p',
+        // 'https://groww.in/p/income-funds',
+        // 'https://groww.in/p/bombay-stock-exchange',
+        // 'https://groww.in/p/hybrid-funds/aggressive-mutual-funds',
+        // 'https://groww.in/p/equity-funds/large-cap-mutual-funds',
+        // 'https://groww.in/p/savings-schemes/uan-universal-account-number',
+        // 'https://groww.in/p/open-ended-funds',
+        // 'https://groww.in/calculators'
+      ],
 
-      numberOfRuns: 1,
+      ci: {
+        collect: {
+          startServerCommand: "npm run start",
 
-      settings: {
-        preset: "desktop",
+          startServerReadyPattern: "ready on",
+
+          url: ["http://localhost:3000"],
+
+          numberOfRuns: 1,
+
+          settings: {
+            preset: "desktop",
+          },
+        },
+
+        assert: {
+          assertions: {
+            "categories:performance": ["warn", { minScore: 0.9 }],
+            "categories:accessibility": ["error", { minScore: 0.9 }],
+          },
+        },
+
+        upload: {
+          target: "filesystem",
+        },
+
+        server: {},
       },
     },
-
-    assert: {
-      assertions: {
-        "categories:performance": ["warn", { minScore: 0.9 }],
-        "categories:accessibility": ["error", { minScore: 0.8 }],
-      },
-    },
-	
-    upload: {
-      target: "temporary-public-storage",
-    },
-
-    server: {},
   },
 };
