2741b3ddb83253c32af056f6fa7d2697f6c4a30c
Abhinab <abhinabpsc@groww.in>
2023-10-11 11:41:06 +0530

    URLS Updated

--- dummy-Template/table.js
+++ dummy-Template/table.js
@@ -1,74 +1,70 @@
+import data from "./reports.json" assert { type: "json" };
 
 function generateTable() {
-    // Get the table container element
-    const URL_APPSCRIPT = "https://script.google.com/macros/s/AKfycbwV5F8v4TT36Lyuey7ho3CAsTC9H7oYglZ61gZ62iyiIg6UIU754V53fl2wiClhgc2d/exec"
-    const URL_SHEETDB = "https://sheetdb.io/api/v1/cq1zv99bs9lz9"
+  // Get the table container element
+  const URL_APPSCRIPT =
+    "https://script.google.com/macros/s/AKfycbzSPzynMuaavoKnC1PLV03VN0afp2x_bQylju3iDQVzzj_uSYWTYwheeLtiio84qQg/exec";
+  const URL_SHEETDB = "https://sheetdb.io/api/v1/cq1zv99bs9lz9";
 
-    const tableContainer = document.querySelector("#table-container");
-    const table = document.querySelector("#table-main");
-   
-      // Iterate over the data and create a row for each object
+  const tableContainer = document.querySelector("#table-container");
+  const table = document.querySelector("#table-main");
+
+  const headerRow = document.createElement("tr");
+  headerRow.innerHTML = `<th  > URL</th><th  >Emulated Form Factor</th><th  >Accessibility</th><th  >Best Practices</th><th  >Performance</th><th  >SEO</th>`;
+  table.appendChild(headerRow);
+  // Iterate over the data and create a row for each object
   const dataAddition = (data) => {
-    console.log(data, "assas")
+    console.log(data, "assas");
     for (const object of data) {
-        const row = document.createElement("tr");
-    
-        row.innerHTML = `<td><a href=${object.url} target="/">${
-          object.url
-        }</a></td><td>${
-          object.emulatedFormFactor
-        }</td><td class="score ${
-          object["scores/accessibility"] > 85
+      const row = document.createElement("tr");
+
+      row.innerHTML = `<td class="score"><a href=${object.url} target="/">${
+        object.url
+      }</a></td><td class="score">${object.emulatedFormFactor}</td><td class="score ${
+        object["accessibility"] > 85
+          ? "score-good"
+          : object["accessibility"] > 75
+          ? "score-average"
+          : "score-bad"
+      }">${object["accessibility"]}</td>
+        <td class="score ${
+          object["bestPractices"] > 85
             ? "score-good"
-            : object["scores/accessibility"] > 75
+            : object["bestPractices"] > 75
             ? "score-average"
             : "score-bad"
-        }">${object["scores/accessibility"]}</td>
+        }">${object["bestPractices"]}</td>
         <td class="score ${
-            object["scores/bestPractices"] > 85
-              ? "score-good"
-              : object["scores/bestPractices"] > 75
-              ? "score-average"
-              : "score-bad"
-          }">${
-          object["scores/bestPractices"]
-        }</td>
-        <td class="score ${
-            object["scores/performance"] > 85
-              ? "score-good"
-              : object["scores/performance"] > 75
-              ? "score-average"
-              : "score-bad"
-          }">${object["scores/performance"]}</td>
+          object["performance"] > 85
+            ? "score-good"
+            : object["performance"] > 75
+            ? "score-average"
+            : "score-bad"
+        }">${object["performance"]}</td>
         
         <td class="score ${
-            object["scores/seo"] > 85
+          object["seo"] > 85
             ? "score-good"
-              : object["scores/seo"] > 75
-              ? "score-average"
-              : "score-bad"
-          }">${object["scores/seo"]}</td>`;
-    
-        table.appendChild(row);
-      }
-  }
+            : object["seo"] > 75
+            ? "score-average"
+            : "score-bad"
+        }">${object["seo"]}</td>`;
 
-    async function logReports() {
-        const response = await fetch(URL_APPSCRIPT);
-        var dataArray = await response.json().then((res) => dataAddition(res.data));
-        console.log(dataArray,"dada");
-        // dataAddition(dataArray.data);
-        return dataArray;
+      table.appendChild(row);
     }
-    logReports();
-    console.log(data)
-    
+  };
+
+  async function logReports() {
+    const response = await fetch(URL_APPSCRIPT);
+    var dataArray = await response.json().then((res) => dataAddition(res.data));
+    console.log(dataArray, "dada");
+    // dataAddition(dataArray.data);
+    return dataArray;
+  }
+  logReports();
+  console.log(data);
+
   // Create a table header row
-  const headerRow = document.createElement("tr");
-  headerRow.innerHTML = `<th scope="col"> URL</th><th scope="col">Emulated Form Factor</th><th scope="col">Accessibility</th><th scope="col">Best Practices</th><th scope="col">Performance</th><th scope="col">SEO</th>`;
-  table.appendChild(headerRow);
-
-
 
   // Append the table to the table container element
 }
